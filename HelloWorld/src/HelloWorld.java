a3cbe74cc4cce5d0a73c8859ddee4505087e6705
Jakob <63636667+jakobfrederikson@users.noreply.github.com>
2021-09-22 12:17:19 +1200

    Add files via upload

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		int a;
+		int b;
+		
+		a = 5;
+		b = 6;
+		
+		System.out.println("5 + 6 = " + a + b); // this somehow works
+		System.out.println("Java scares me.");
+	}
+
+}
